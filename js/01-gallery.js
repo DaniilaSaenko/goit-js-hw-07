cefa44127194b22e22267e00298039ac408d1bd5
DaniilaSaenko <daniila.s777@gmail.com>
2022-11-16 19:09:04 +0200

    JS-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,6 +19,24 @@ const markUp = galleryItems
   )
   .join("");
 
+/* const markUp = galleryItems.reduce(
+  (acc, { original, preview, description }) => {
+    return `
+    ${acc}
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    },
+  ""
+); */
+
 const gallery = document.querySelector(".gallery");
 
 gallery.insertAdjacentHTML("beforeend", markUp);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,28 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
+const markUp = galleryItems.reduce(
+  (acc, { original, preview, description }) => {
+    return `
+    ${acc}
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+  },
+  ""
+);
 
-const markUp = galleryItems
-  .map(
-    (item) =>
-      `<a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-       data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>`
-  )
-    .join("");
-  
-    const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector(".gallery");
 
 gallery.insertAdjacentHTML("beforeend", markUp);
-    
+
 new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250
-})
+  captionsData: "alt",
+  captionDelay: 250,
+});
